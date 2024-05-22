c21d7ecfb5a0e617f8b7265b0872f5f7c2803179
Tamás Tóth <tomjtoth@gmail.com>
2024-05-22 14:39:00 +0300

    13.5: refactored according to course material

--- src/models/blog.js
+++ src/models/blog.js
@@ -1,8 +1,9 @@
-const { Sequelize, Model, DataTypes } = require("sequelize");
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const { sequelize } = require("../utils/db");
 
 class Blog extends Model {}
+
 Blog.init(
   {
     id: {
