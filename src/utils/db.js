200c301ba37c8f385b3685149f374866cdbe94ab
Tamás Tóth <tomjtoth@gmail.com>
2024-05-23 11:20:15 +0300

    13.17: db recreated + added timestamps for blogs via migrations

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,11 +1,34 @@
 const Sequelize = require("sequelize");
+const { Umzug, SequelizeStorage } = require("umzug");
 const { DATABASE_URL } = require("./config");
 
 const sequelize = new Sequelize(DATABASE_URL);
 
+const migrationConf = {
+  migrations: { glob: "src/migrations/*.js" },
+  storage: new SequelizeStorage({ sequelize, tableName: "migrations" }),
+  context: sequelize.getQueryInterface(),
+  logger: console,
+};
+
+const runMigrations = async () => {
+  const migrator = new Umzug(migrationConf);
+  const migrations = await migrator.up();
+  console.log("Migrations up to date", {
+    files: migrations.map((mig) => mig.name),
+  });
+};
+
+const rollbackMigration = async () => {
+  await sequelize.authenticate();
+  const migrator = new Umzug(migrationConf);
+  await migrator.down();
+};
+
 const connectToDatabase = async () => {
   try {
     await sequelize.authenticate();
+    await runMigrations();
     console.log("database connected");
   } catch (err) {
     console.log("connecting database failed");
@@ -15,4 +38,4 @@ const connectToDatabase = async () => {
   return null;
 };
 
-module.exports = { connectToDatabase, sequelize };
+module.exports = { connectToDatabase, sequelize, rollbackMigration };
