9845c189ac29024ad0a96b02444c0b82c3046653
Tamás Tóth <tomjtoth@gmail.com>
2025-03-24 15:39:16 +0200

    storing created token on login

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -4,6 +4,7 @@ const router = require("express").Router();
 const { SECRET } = require("../utils/config");
 const { AuthErr } = require("../utils/AuthErr");
 const User = require("../models/user");
+const { Session } = require("../models");
 
 router.post("/", async ({ body: { username, password } }, res) => {
   if (!username) throw new AuthErr("missing username", 400);
@@ -18,12 +19,16 @@ router.post("/", async ({ body: { username, password } }, res) => {
   if (!user || !passwordCorrect)
     throw new AuthErr("invalid username or password!");
 
+  if (user.disabled) throw new AuthErr("your account has been disabled", 403);
+
   const userForToken = {
     username,
     id: user.id,
   };
 
   const token = jwt.sign(userForToken, SECRET);
+
+  await Session.create({ token });
 
   res.status(200).send({ token, username: user.username, name: user.name });
 });
